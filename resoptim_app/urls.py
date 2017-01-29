a3d97740434c7c4402c143013e1228a082375086
Jayden Windle <jaydenwindle@gmail.com>
2017-01-29 05:46:21 -0500

    added delete work entry and passing all entries to dashboard

--- resoptim_app/urls.py
+++ resoptim_app/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib.auth import views as auth_views
 from django.contrib import admin
-from views import WorkEntryUpdate, WorkEntryCreate
+from views import WorkEntryUpdate, WorkEntryCreate, WorkEntryDelete
 
 admin.autodiscover()
 
@@ -11,6 +11,7 @@ urlpatterns = [
     url(r'^$', views.index, name='dashboard'),
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
-    url(r'^update_work_entry/(?P<pk>[\w-]+)$', WorkEntryUpdate.as_view(), name='update_work_entry'),
     url(r'^create_work_entry/$', WorkEntryCreate.as_view(), name='create_work_entry'),
+    url(r'^update_work_entry/(?P<pk>[\w-]+)$', WorkEntryUpdate.as_view(), name='update_work_entry'),
+    url(r'^delete_work_entry/(?P<pk>[\w-]+)$', WorkEntryDelete.as_view(), name='delete_work_entry'),
 ]
--- resoptim_app/views.py
+++ resoptim_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.views.generic import UpdateView, CreateView
+from django.views.generic import UpdateView, CreateView, DeleteView
 from models import WorkEntry 
 
 # Create your views here.
@@ -9,7 +9,10 @@ def index(request):
     if request.user.is_authenticated():
         return render(request, 'dashboard.html', {
             'user': request.user,
-            'work_entries': request.user.work.all()
+            'work_entries': request.user.work.all(),
+            'education_entries': request.user.education.all(),
+            'social_profiles': request.user.social.all(),
+            'skill_entries': request.user.skills.all()
         })
     else: 
         return redirect('login')
@@ -25,6 +28,17 @@ class WorkEntryUpdate(UpdateView):
     fields = ['company', 'position', 'summary']
     template_name = 'generic_form.html'
     success_url = '/app/'
+
+class WorkEntryDelete(DeleteView):
+    model = WorkEntry 
+    template_name = 'delete_work_entry.html'
+    success_url = '/app/'
+    def get_object(self, queryset=None):
+        """ Hook to ensure object is owned by request.user. """
+        obj = super(WorkEntryDelete, self).get_object()
+        if not obj.owner == self.request.user:
+            raise Http404
+        return obj
 
 def landingPage(request):
     return render(request, 'index.html', {
