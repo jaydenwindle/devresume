38364b14f78786ce9fb4d25e2fe5e7159dfc8fbd
Jayden Windle <jaydenwindle@gmail.com>
2017-02-09 14:06:44 -0500

    fixed breaking build by removing ai from static dirs

--- resoptim/settings.py
+++ resoptim/settings.py
@@ -129,7 +129,6 @@ STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
-    os.path.join(PROJECT_ROOT, 'ai/static'),
 )
 
 # Simplified static file serving.
